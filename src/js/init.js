eff4fc811cd9d5dbd7b975d1f017e7906653dd2f
Nicolas Hoizey <nicolas@hoizey.com>
2014-10-17 12:00:35 +0200

    No more JavaScript dependency to load CSS and apply breakpoints

--- src/js/init.js
+++ src/js/init.js
@@ -9,12 +9,12 @@
   skel.init({
     reset: 'full',
     breakpoints: {
-      'global': { range: '*', href: 'css/style.css', containers: 1400, grid: { gutters: 40 }, viewport: { scalable: false } },
-      'wide':   { range: '961-1880', href: 'css/style-wide.css', containers: 1200, grid: { gutters: 40 } },
-      'normal': { range: '961-1620', href: 'css/style-normal.css', containers: 960, grid: { gutters: 40 } },
-      'narrow': { range: '961-1320', href: 'css/style-narrow.css', containers: '100%', grid: { gutters: 20 } },
-      'narrower': { range: '-960', href: 'css/style-narrower.css', containers: '100%', grid: { gutters: 15 } },
-      'mobile': { range: '-736', href: 'css/style-mobile.css', grid: { collapse: true } }
+      'global': { range: '*', containers: 1400, grid: { gutters: 40 }, viewport: { scalable: false } },
+      'wide':   { range: '961-1880', containers: 1200, grid: { gutters: 40 } },
+      'normal': { range: '961-1620', containers: 960, grid: { gutters: 40 } },
+      'narrow': { range: '961-1320', containers: '100%', grid: { gutters: 20 } },
+      'narrower': { range: '-960', containers: '100%', grid: { gutters: 15 } },
+      'mobile': { range: '-736', grid: { collapse: true } }
     },
     plugins: {
       layers: {
